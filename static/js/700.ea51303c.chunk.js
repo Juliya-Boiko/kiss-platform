af78d1e62247bb89f29585012f2193099695df6d
Juliya-Boiko <90456413+Juliya-Boiko@users.noreply.github.com>
2023-02-27 22:40:51 +0000

    Deploying to gh-pages from @ Juliya-Boiko/kiss-platform@980caa6695aceff55b6374d4625bae8b561bdefe 🚀

---
+++ static/js/700.ea51303c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[700],{7700:function(e,t,n){n.r(t);var r=n(9434),s=n(7649),a=n(3329);t.default=function(){var e=(0,r.I0)();return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:"Change Password Form"}),(0,a.jsx)("input",{type:"text"}),(0,a.jsx)("button",{type:"submit",onClick:function(t){t.preventDefault(),console.log("Password changed"),e((0,s.Cp)())},children:"Change password"})]})}}}]);
+//# sourceMappingURL=700.ea51303c.chunk.js.map
