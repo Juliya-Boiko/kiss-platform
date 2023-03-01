1ed95eb40577560d80a2df4a4fc9145a92a5139c
Juliya-Boiko <90456413+Juliya-Boiko@users.noreply.github.com>
2023-03-01 13:38:51 +0000

    Deploying to gh-pages from @ Juliya-Boiko/kiss-platform@3a705a1ade28de53d3b37fd82daf0c6ef9c8538f 🚀

--- static/js/459.a6421c13.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[459],{459:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"CreatePage"})}}}]);
-//# sourceMappingURL=459.a6421c13.chunk.js.map
